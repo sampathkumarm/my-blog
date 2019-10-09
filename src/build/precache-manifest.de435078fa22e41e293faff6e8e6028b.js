ce207d9d2180fe86fafe59fd44e0446f878202af
sgm <msampathkumar@gmail.com>
2019-10-09 00:12:51 -0700

    MyBlog first commit.

---
+++ src/build/precache-manifest.de435078fa22e41e293faff6e8e6028b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad0ce97d24dfcaf7548ecb2237ea315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f80f3fee0345c533db5",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "d24d32e38b5cf0744078",
+    "url": "/static/js/2.03f99ea2.chunk.js"
+  },
+  {
+    "revision": "2f80f3fee0345c533db5",
+    "url": "/static/js/main.2bcf278b.chunk.js"
+  },
+  {
+    "revision": "afc0a052c644dce5e6bc",
+    "url": "/static/js/runtime-main.fbd335c9.js"
+  }
+]);
